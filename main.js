0d114250d9d3d27f304b783f4425a4b628365acf
Elysium1436 <bryan.af7@gmail.com>
2020-09-09 01:23:27 -0300

    added functions for event and event listener (nw)

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function tab1show() {
   let tab1_content = document.getElementById("tab1-content");
   let tab2_content = document.getElementById("tab2-content");
 
-  tab1_content.style.display = "block";
+  tab1_content.style.display = "flex";
   tab2_content.style.display = "none";
 }
 
@@ -10,6 +10,12 @@ function tab2show() {
   let tab2_content = document.getElementById("tab2-content");
   let tab1_content = document.getElementById("tab1-content");
 
-  tab2_content.style.display = "block";
+  tab2_content.style.display = "flex";
   tab1_content.style.display = "none";
 }
+
+let tab1 = document.getElementById("tab1");
+let tab2 = document.getElementById("tab2");
+
+tab1.addEventListener(onclick, tab1show);
+tab2.addEventListener(onclick, tab2show);
